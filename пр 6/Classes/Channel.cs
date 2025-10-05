64451c8dfb9644b425329d495d9d9b40dcd79212
yankabelosludtseva <nblsldcv@gmail.com>
2025-10-05 13:18:52 +0500

    Разработанный класс. Белослудцева

---
+++ пр 6/Classes/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_6.Classes
+{
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
